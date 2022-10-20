dc36d9311339b9650127578342112cc3eb398407
Clement Grihon <gclems@gmail.com>
2022-10-20 15:24:46 +0200

    get token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  const token = core.getInput("token");
   console.log("OH HELLO :D");
 } catch (error) {
   core.setFailed(error.message);
