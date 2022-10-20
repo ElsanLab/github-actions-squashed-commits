ba65debd23c88f61eb211f25bf6d244ee2501b5b
Clement Grihon <gclems@gmail.com>
2022-10-20 16:23:50 +0200

    correct env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 
 try {
   const token = core.getInput("token");
@@ -8,37 +8,35 @@ try {
     core.setFailed("NO TOKEN GIVEN");
   }
 
-  console.log("REPO", process.env.GITHUB_REPOSITORY);
+  const repo = process.env.GITHUB_REPOSITORY;
 
-  //   const repo = github.getEnv("GITHUB_REPOSITORY");
+  const pullRequestNumber = +process.env.GITHUB_REF_NAME.split("/")[0];
+  if (!pullRequestNumber) {
+    core.setFailed("UNABLE TO GET PR NUMBER");
+  }
 
-  //   const pullRequestNumber = +github.getEnv("GITHUB_REF_NAME").split("/")[0];
-  //   if (!pullRequestNumber) {
-  //     core.setFailed("UNABLE TO GET PR NUMBER");
-  //   }
+  if (
+    process.env.GITHUB_REF_TYPE !== "branch" ||
+    process.env.GITHUB_EVENT_NAME !== "pull_request"
+  ) {
+    core.setFailed("THIS ACTION CAN ONLY BE TRIGGERED ON A PULL REQUEST");
+  }
 
-  //   if (
-  //     github.getEnv("GITHUB_REF_TYPE") !== "branch" ||
-  //     github.getEnv("GITHUB_EVENT_NAME") !== "pull_request"
-  //   ) {
-  //     core.setFailed("THIS ACTION CAN ONLY BE TRIGGERED ON A PULL REQUEST");
-  //   }
+  const headers = new Headers({
+    Accept: "application/vnd.github+json",
+    Authorization: `token ${token}`,
+  });
+  const opts = {
+    method: method,
+    headers: headers,
+  };
 
-  //   const headers = new Headers({
-  //     Accept: "application/vnd.github+json",
-  //     Authorization: `token ${token}`,
-  //   });
-  //   const opts = {
-  //     method: method,
-  //     headers: headers,
-  //   };
-
-  //   fetch(
-  //     `https://api.github.com/repos/${repo}/pulls/${pullRequestNumber}`,
-  //     opts
-  //   ).then((response) => {
-  //     console.log("RESPONSE", response);
-  //   });
+  fetch(
+    `https://api.github.com/repos/${repo}/pulls/${pullRequestNumber}`,
+    opts
+  ).then((response) => {
+    console.log("RESPONSE", response);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
