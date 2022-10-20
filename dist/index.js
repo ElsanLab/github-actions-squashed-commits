d3953318da864c7dcc0661e4d47fe5d825e40b27
Clement Grihon <gclems@gmail.com>
2022-10-20 16:20:12 +0200

    built

--- dist/index.js
+++ dist/index.js
@@ -9694,6 +9694,8 @@ try {
     core.setFailed("NO TOKEN GIVEN");
   }
 
+  console.log("REPO", process.env.GITHUB_REPOSITORY);
+
   const repo = github.getEnv("GITHUB_REPOSITORY");
 
   const pullRequestNumber = +github.getEnv("GITHUB_REF_NAME").split("/")[0];
